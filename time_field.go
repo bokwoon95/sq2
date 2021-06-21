62c274a6fdf027b54660093cb97c9a3bfeb4a410
bokwoon <bokwoon.c@gmail.com>
2021-06-21 17:48:49 +0800

    checkpoint

--- time_field.go
+++ time_field.go
@@ -6,15 +6,21 @@ import (
 )
 
 type TimeField struct {
-	GenericField
-	Format string
-	Values []interface{}
+	info FieldInfo
 }
 
 var _ Field = TimeField{}
 
+func (f TimeField) AppendSQLExclude(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int, excludedTableQualifiers []string) error {
+	return f.info.AppendSQLExclude(dialect, buf, args, params, excludedTableQualifiers)
+}
+
+func (f TimeField) GetAlias() string { return f.info.FieldAlias }
+
+func (f TimeField) GetName() string { return f.info.FieldName }
+
 func NewTimeField(fieldName string, tableInfo TableInfo) TimeField {
-	return TimeField{GenericField: GenericField{
+	return TimeField{info: FieldInfo{
 		TableSchema: tableInfo.TableSchema,
 		TableName:   tableInfo.TableName,
 		TableAlias:  tableInfo.TableAlias,
@@ -23,59 +29,46 @@ func NewTimeField(fieldName string, tableInfo TableInfo) TimeField {
 }
 
 func TimeFieldf(format string, values ...interface{}) TimeField {
-	return TimeField{Format: format, Values: values}
+	return TimeField{info: FieldInfo{
+		Format: format,
+		Values: values,
+	}}
 }
 
 func (f TimeField) As(alias string) TimeField {
-	f.FieldAlias = alias
+	f.info.FieldAlias = alias
 	return f
 }
 
 func (f TimeField) Asc() TimeField {
-	f.Descending.Valid = true
-	f.Descending.Bool = false
+	f.info.Descending.Valid = true
+	f.info.Descending.Bool = false
 	return f
 }
 
 func (f TimeField) Desc() TimeField {
-	f.Descending.Valid = true
-	f.Descending.Bool = true
+	f.info.Descending.Valid = true
+	f.info.Descending.Bool = true
 	return f
 }
 
 func (f TimeField) NullsLast() TimeField {
-	f.Nullsfirst.Valid = true
-	f.Nullsfirst.Bool = false
+	f.info.NullsFirst.Valid = true
+	f.info.NullsFirst.Bool = false
 	return f
 }
 
 func (f TimeField) NullsFirst() TimeField {
-	f.Nullsfirst.Valid = true
-	f.Nullsfirst.Bool = true
+	f.info.NullsFirst.Valid = true
+	f.info.NullsFirst.Bool = true
 	return f
-}
-
-func (f TimeField) AppendSQLExclude(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int, excludedTableQualifiers []string) error {
-	if f.Format != "" {
-		err := BufferPrintf(dialect, buf, args, params, excludedTableQualifiers, f.Format, f.Values)
-		if err != nil {
-			return err
-		}
-		f.TableSchema, f.TableName, f.TableAlias, f.FieldName, f.FieldAlias = "", "", "", "", ""
-	}
-	return f.GenericField.AppendSQLExclude(dialect, buf, args, params, excludedTableQualifiers)
 }
 
 func (f TimeField) IsNull() Predicate { return IsNull(f) }
 
 func (f TimeField) IsNotNull() Predicate { return IsNotNull(f) }
 
-func (f TimeField) In(v interface{}) Predicate {
-	if v, ok := v.(RowValue); ok {
-		return Predicatef("{} IN {}", f, v)
-	}
-	return Predicatef("{} IN ({})", f, v)
-}
+func (f TimeField) In(v interface{}) Predicate { return In(f, v) }
 
 func (f TimeField) Eq(field TimeField) Predicate { return Eq(f, field) }
 
