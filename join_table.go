2d51f7cb53379b6033e4a86dfc1f0a62ec6a9444
bokwoon <bokwoon.c@gmail.com>
2021-06-21 19:36:39 +0800

    checkpoint

--- join_table.go
+++ join_table.go
@@ -1,128 +1,134 @@
 package sq
 
-// import "bytes"
-//
-// type JoinType string
-//
-// const (
-// 	JoinTypeInner JoinType = "JOIN"
-// 	JoinTypeLeft  JoinType = "LEFT JOIN"
-// 	JoinTypeRight JoinType = "RIGHT JOIN"
-// 	JoinTypeFull  JoinType = "FULL JOIN"
-// )
-//
-// type JoinTable struct {
-// 	JoinType     JoinType
-// 	Table        Table
-// 	OnPredicates VariadicPredicate
-// }
-//
-// var _ SQLAppender = JoinTable{}
-//
-// func Join(table Table, predicates ...Predicate) JoinTable {
-// 	return JoinTable{
-// 		JoinType: JoinTypeInner,
-// 		Table:    table,
-// 		OnPredicates: VariadicPredicate{
-// 			Predicates: predicates,
-// 		},
-// 	}
-// }
-//
-// func LeftJoin(table Table, predicates ...Predicate) JoinTable {
-// 	return JoinTable{
-// 		JoinType: JoinTypeLeft,
-// 		Table:    table,
-// 		OnPredicates: VariadicPredicate{
-// 			Predicates: predicates,
-// 		},
-// 	}
-// }
-//
-// func RightJoin(table Table, predicates ...Predicate) JoinTable {
-// 	return JoinTable{
-// 		JoinType: JoinTypeRight,
-// 		Table:    table,
-// 		OnPredicates: VariadicPredicate{
-// 			Predicates: predicates,
-// 		},
-// 	}
-// }
-//
-// func FullJoin(table Table, predicates ...Predicate) JoinTable {
-// 	return JoinTable{
-// 		JoinType: JoinTypeFull,
-// 		Table:    table,
-// 		OnPredicates: VariadicPredicate{
-// 			Predicates: predicates,
-// 		},
-// 	}
-// }
-//
-// func CustomJoin(joinType JoinType, table Table, predicates ...Predicate) JoinTable {
-// 	return JoinTable{
-// 		JoinType: joinType,
-// 		Table:    table,
-// 		OnPredicates: VariadicPredicate{
-// 			Predicates: predicates,
-// 		},
-// 	}
-// }
-//
-// func (join JoinTable) AppendSQL(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int) error {
-// 	if join.JoinType == "" {
-// 		join.JoinType = JoinTypeInner
-// 	}
-// 	buf.WriteString(string(join.JoinType) + " ")
-// 	var err error
-// 	switch v := join.Table.(type) {
-// 	case nil:
-// 		buf.WriteString("NULL")
-// 	case Subquery:
-// 		buf.WriteString("(")
-// 		err = v.AppendSQL("", buf, args, nil)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		buf.WriteString(")")
-// 	default:
-// 		err = v.AppendSQL("", buf, args, nil)
-// 		if err != nil {
-// 			return err
-// 		}
-// 	}
-// 	if join.Table != nil {
-// 		alias := join.Table.GetAlias()
-// 		if alias != "" {
-// 			buf.WriteString(" AS ")
-// 			buf.WriteString(alias)
-// 		}
-// 	}
-// 	if len(join.OnPredicates.Predicates) > 0 {
-// 		buf.WriteString(" ON ")
-// 		join.OnPredicates.Toplevel = true
-// 		err = join.OnPredicates.AppendSQLExclude("", buf, args, nil, nil)
-// 		if err != nil {
-// 			return err
-// 		}
-// 	}
-// 	return nil
-// }
-//
-// type JoinTables []JoinTable
-//
-// var _ SQLAppender = JoinTables{}
-//
-// func (joins JoinTables) AppendSQL(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int) error {
-// 	var err error
-// 	for i, join := range joins {
-// 		if i > 0 {
-// 			buf.WriteString(" ")
-// 		}
-// 		err = join.AppendSQL(dialect, buf, args, nil)
-// 		if err != nil {
-// 			return err
-// 		}
-// 	}
-// 	return nil
-// }
+import (
+	"bytes"
+	"fmt"
+)
+
+type JoinType string
+
+const (
+	JoinTypeInner    JoinType = "JOIN"
+	JoinTypeLeft     JoinType = "LEFT JOIN"
+	JoinTypeRight    JoinType = "RIGHT JOIN"
+	JoinTypeFull     JoinType = "FULL JOIN"
+	JoinTypeCross    JoinType = "CROSS JOIN"
+	JoinTypeImplicit JoinType = ","
+)
+
+type JoinTable struct {
+	JoinType     JoinType
+	Table        Table
+	OnPredicates VariadicPredicate
+}
+
+var _ SQLAppender = JoinTable{}
+
+func Join(table Table, predicates ...Predicate) JoinTable {
+	return JoinTable{
+		JoinType:     JoinTypeInner,
+		Table:        table,
+		OnPredicates: VariadicPredicate{Predicates: predicates},
+	}
+}
+
+func LeftJoin(table Table, predicates ...Predicate) JoinTable {
+	return JoinTable{
+		JoinType:     JoinTypeLeft,
+		Table:        table,
+		OnPredicates: VariadicPredicate{Predicates: predicates},
+	}
+}
+
+func RightJoin(table Table, predicates ...Predicate) JoinTable {
+	return JoinTable{
+		JoinType:     JoinTypeRight,
+		Table:        table,
+		OnPredicates: VariadicPredicate{Predicates: predicates},
+	}
+}
+
+func FullJoin(table Table, predicates ...Predicate) JoinTable {
+	return JoinTable{
+		JoinType:     JoinTypeFull,
+		Table:        table,
+		OnPredicates: VariadicPredicate{Predicates: predicates},
+	}
+}
+
+func CrossJoin(table Table) JoinTable {
+	return JoinTable{
+		JoinType: JoinTypeCross,
+		Table:    table,
+	}
+}
+
+func ImplicitJoin(table Table) JoinTable {
+	return JoinTable{
+		JoinType: JoinTypeImplicit,
+		Table:    table,
+	}
+}
+
+func CustomJoin(joinType JoinType, table Table, predicates ...Predicate) JoinTable {
+	return JoinTable{
+		JoinType:     joinType,
+		Table:        table,
+		OnPredicates: VariadicPredicate{Predicates: predicates},
+	}
+}
+
+func (join JoinTable) AppendSQL(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int) error {
+	if join.JoinType == "" {
+		join.JoinType = JoinTypeInner
+	}
+	buf.WriteString(string(join.JoinType) + " ")
+	var err error
+	switch v := join.Table.(type) {
+	case nil:
+		return fmt.Errorf("sq: joining on a nil table")
+	case Subquery:
+		buf.WriteString("(")
+		err = v.AppendSQL(dialect, buf, args, params)
+		if err != nil {
+			return err
+		}
+		buf.WriteString(")")
+	default:
+		err = v.AppendSQL(dialect, buf, args, params)
+		if err != nil {
+			return err
+		}
+	}
+	if tableAlias := join.Table.GetAlias(); tableAlias != "" {
+		buf.WriteString(" AS ")
+		buf.WriteString(QuoteIdentifier(dialect, tableAlias))
+	}
+	if len(join.OnPredicates.Predicates) > 0 {
+		buf.WriteString(" ON ")
+		join.OnPredicates.Toplevel = true
+		err = join.OnPredicates.AppendSQLExclude(dialect, buf, args, params, nil)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type JoinTables []JoinTable
+
+var _ SQLAppender = JoinTables{}
+
+func (joins JoinTables) AppendSQL(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int) error {
+	var err error
+	for i, join := range joins {
+		if i > 0 {
+			buf.WriteString(" ")
+		}
+		err = join.AppendSQL(dialect, buf, args, params)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- table_info.go
+++ table_info.go
@@ -34,3 +34,22 @@ func (tbl TableInfo) GetName() string { return tbl.TableName }
 func (tbl TableInfo) GetSchema() string { return tbl.TableSchema }
 
 func (tbl TableInfo) GetTableInfo() (TableInfo, error) { return tbl, nil }
+
+type customTable struct {
+	format string
+	values []interface{}
+}
+
+var _ Table = customTable{}
+
+func Tablef(format string, values ...interface{}) customTable {
+	return customTable{format: format, values: values}
+}
+
+func (tbl customTable) GetAlias() string { return "" }
+
+func (tbl customTable) GetName() string { return "" }
+
+func (tbl customTable) AppendSQL(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int) error {
+	return BufferPrintf(dialect, buf, args, params, nil, tbl.format, tbl.values)
+}
