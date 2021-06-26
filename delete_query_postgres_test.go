7f70b02685d209354b625c04e60b2f652888e7d7
bokwoon <bokwoon.c@gmail.com>
2021-06-26 18:16:58 +0800

    checkpoint

---
+++ delete_query_postgres_test.go
@@ -0,0 +1,54 @@
+package sq_test
+
+import (
+	"testing"
+
+	. "github.com/bokwoon95/sq"
+)
+
+func Test_PostgresDeleteQuery(t *testing.T) {
+	type TT struct {
+		dialect   string
+		item      Query
+		wantQuery string
+		wantArgs  []interface{}
+	}
+
+	assert := func(t *testing.T, tt TT) {
+		gotQuery, gotArgs, _, err := ToSQL(tt.dialect, tt.item)
+		if err != nil {
+			t.Fatal(testcallers(), err)
+		}
+		if diff := testdiff(gotQuery, tt.wantQuery); diff != "" {
+			t.Error(testcallers(), diff)
+		}
+		if diff := testdiff(gotArgs, tt.wantArgs); diff != "" {
+			t.Error(testcallers(), diff)
+		}
+	}
+
+	t.Run("joins", func(t *testing.T) {
+		ACTOR := NEW_ACTOR("a")
+		t.Parallel()
+		var tt TT
+		tt.item = Postgres.
+			DeleteFrom(ACTOR).
+			Using(ACTOR).
+			Join(ACTOR, Eq(1, 1)).
+			LeftJoin(ACTOR, Eq(1, 1)).
+			RightJoin(ACTOR, Eq(1, 1)).
+			FullJoin(ACTOR, Eq(1, 1)).
+			CrossJoin(ACTOR).
+			CustomJoin("NATURAL JOIN", ACTOR)
+		tt.wantQuery = "DELETE FROM actor AS a" +
+			" USING actor AS a" +
+			" JOIN actor AS a ON $1 = $2" +
+			" LEFT JOIN actor AS a ON $3 = $4" +
+			" RIGHT JOIN actor AS a ON $5 = $6" +
+			" FULL JOIN actor AS a ON $7 = $8" +
+			" CROSS JOIN actor AS a" +
+			" NATURAL JOIN actor AS a"
+		tt.wantArgs = []interface{}{1, 1, 1, 1, 1, 1, 1, 1}
+		assert(t, tt)
+	})
+}
