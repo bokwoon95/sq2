55faf1726a664130f22e7a79a48a4f71d90b1233
bokwoon <bokwoon.c@gmail.com>
2021-06-29 00:22:23 +0800

    checkpoint

--- subquery.go
+++ subquery.go
@@ -3,97 +3,158 @@ package sq
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 type SubqueryField struct {
-	valid      bool
-	query      Query
-	queryAlias string
-	fieldName  string
-	fieldAlias string
+	stickyErr  error
+	fieldNames []string
+	fieldCache map[string]int
+	info       FieldInfo
 }
 
 var _ Field = SubqueryField{}
 
-func (f SubqueryField) GetAlias() string { return f.fieldAlias }
+func (f SubqueryField) GetAlias() string { return f.info.FieldAlias }
 
-func (f SubqueryField) GetName() string { return f.fieldName }
+func (f SubqueryField) GetName() string { return f.info.FieldName }
+
+func (f SubqueryField) AppendSQLExclude(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int, excludedTableQualifiers []string) error {
+	if f.info.TableAlias == "" && (dialect == DialectPostgres || dialect == DialectMySQL) {
+		return fmt.Errorf("subquery field %s invalid because %s subquery needs an alias", f.info.FieldName, dialect)
+	}
+	if _, ok := f.fieldCache[f.info.FieldName]; !ok {
+		if f.stickyErr != nil {
+			return fmt.Errorf("subquery field %s.%s invalid due to Subquery error: %w", f.info.TableAlias, f.info.FieldName, f.stickyErr)
+		} else {
+			return fmt.Errorf("subquery field %s.%s does not exist (available fields: %s)", f.info.TableAlias, f.info.FieldName, strings.Join(f.fieldNames, ", "))
+		}
+	}
+	return f.info.AppendSQLExclude(dialect, buf, args, params, excludedTableQualifiers)
+}
 
 func (f SubqueryField) As(alias string) SubqueryField {
-	f.fieldAlias = alias
+	f.info.FieldAlias = alias
 	return f
 }
 
-func (f SubqueryField) AppendSQLExclude(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int, excludedTableQualifiers []string) error {
-	if !f.valid {
-		return fmt.Errorf("referenced nonexistent SubqueryField")
-	}
-	if f.fieldName == "" {
-		return fmt.Errorf("SubqueryField has no name")
-	}
-	if f.queryAlias != "" {
-		buf.WriteString(f.queryAlias + ".")
-	}
-	buf.WriteString(f.fieldName)
-	return nil
+func (f SubqueryField) Asc() SubqueryField {
+	f.info.Descending.Valid = true
+	f.info.Descending.Bool = false
+	return f
 }
 
-type Subquery map[string]SubqueryField
+func (f SubqueryField) Desc() SubqueryField {
+	f.info.Descending.Valid = true
+	f.info.Descending.Bool = true
+	return f
+}
+
+func (f SubqueryField) NullsLast() SubqueryField {
+	f.info.NullsFirst.Valid = true
+	f.info.NullsFirst.Bool = false
+	return f
+}
+
+func (f SubqueryField) NullsFirst() SubqueryField {
+	f.info.NullsFirst.Valid = true
+	f.info.NullsFirst.Bool = true
+	return f
+}
+
+func (f SubqueryField) IsNull() Predicate { return IsNull(f) }
+
+func (f SubqueryField) IsNotNull() Predicate { return IsNotNull(f) }
+
+func (f SubqueryField) In(v interface{}) Predicate { return In(f, v) }
+
+func (f SubqueryField) Eq(v interface{}) Predicate { return Eq(f, v) }
+
+func (f SubqueryField) Ne(v interface{}) Predicate { return Ne(f, v) }
+
+func (f SubqueryField) Gt(v interface{}) Predicate { return Gt(f, v) }
+
+func (f SubqueryField) Ge(v interface{}) Predicate { return Ge(f, v) }
+
+func (f SubqueryField) Lt(v interface{}) Predicate { return Lt(f, v) }
+
+func (f SubqueryField) Le(v interface{}) Predicate { return Le(f, v) }
+
+type Subquery struct {
+	query          Query
+	stickyErr      error
+	explicitFields bool
+	subqueryAlias  string
+	fieldNames     []string
+	fieldCache     map[string]int
+}
 
 var _ Table = Subquery{}
 
-func NewSubquery(query Query, alias string) (Subquery, error) {
-	if query == nil {
-		return nil, fmt.Errorf("Subquery query cannot be nil")
+func NewSubquery(alias string, query Query) Subquery {
+	q := Subquery{
+		query:         query,
+		subqueryAlias: alias,
+		fieldCache:    make(map[string]int),
 	}
-	q := Subquery{"": {
-		query:      query,
-		queryAlias: alias,
-	}}
+	if query == nil {
+		q.stickyErr = fmt.Errorf("subquery query cannot be nil")
+		return q
+	}
 	fields, err := query.GetFetchableFields()
 	if err != nil {
-		return nil, fmt.Errorf("error fetching fields for Subquery: %w", err)
+		q.stickyErr = fmt.Errorf("subquery %s failed to fetch query fields: %w", q.subqueryAlias, err)
+		return q
 	}
 	if len(fields) == 0 {
-		return nil, fmt.Errorf("Subquery query does not return any fields")
+		q.stickyErr = fmt.Errorf("subquery %s does not return any fields", q.subqueryAlias)
+		return q
 	}
 	for i, field := range fields {
-		if field == nil {
-			return nil, fmt.Errorf("field #%d in Subquery query is nil", i+1)
-		}
 		fieldName := field.GetAlias()
 		if fieldName == "" {
 			fieldName = field.GetName()
 		}
 		if fieldName == "" {
-			return q, fmt.Errorf("field #%d in Subquery has no name and no alias", i+1)
+			q.stickyErr = fmt.Errorf("subquery %s field #%d needs a name or an alias", q.subqueryAlias, i+1)
+			return q
 		}
-		q[fieldName] = SubqueryField{
-			valid:      true,
-			queryAlias: alias,
-			fieldName:  fieldName,
-		}
+		q.fieldNames = append(q.fieldNames, fieldName)
 	}
-	return q, nil
+	for i, fieldName := range q.fieldNames {
+		q.fieldCache[fieldName] = i
+	}
+	return q
 }
 
-func (q Subquery) GetAlias() string { return q[""].queryAlias }
+func (q Subquery) GetAlias() string { return q.subqueryAlias }
 
 func (q Subquery) GetName() string { return "" }
 
 func (q Subquery) AppendSQL(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int) error {
-	if len(q) == 0 {
-		return fmt.Errorf("empty Subquery")
-	}
-	query := q[""].query
-	if query == nil {
-		return fmt.Errorf("empty Subquery")
+	if q.stickyErr != nil {
+		return q.stickyErr
 	}
 	buf.WriteString("(")
-	err := query.AppendSQL(dialect, buf, args, params)
+	err := q.query.AppendSQL(dialect, buf, args, params)
 	if err != nil {
-		return err
+		return fmt.Errorf("subquery failed to build query: %w", err)
 	}
 	buf.WriteString(")")
+	if q.subqueryAlias == "" && (dialect == DialectPostgres || dialect == DialectMySQL) {
+		return fmt.Errorf("%s subquery need an alias", dialect)
+	}
 	return nil
+}
+
+func (q Subquery) Field(fieldName string) SubqueryField {
+	return SubqueryField{
+		stickyErr:  q.stickyErr,
+		fieldNames: q.fieldNames,
+		fieldCache: q.fieldCache,
+		info: FieldInfo{
+			TableAlias: q.subqueryAlias,
+			FieldName:  fieldName,
+		},
+	}
 }
---
+++ subquery_test.go
@@ -0,0 +1,104 @@
+package sq
+
+import (
+	"testing"
+)
+
+func TestSubquery(t *testing.T) {
+	type TT struct {
+		dialect    string
+		item       SQLAppender
+		wantQuery  string
+		wantArgs   []interface{}
+		wantParams map[string][]int
+	}
+
+	assert := func(t *testing.T, tt TT) {
+		gotQuery, gotArgs, gotParams, err := ToSQL("", tt.item)
+		if err != nil {
+			t.Fatal(Callers(), err)
+		}
+		if diff := Diff(tt.wantQuery, gotQuery); diff != "" {
+			t.Error(Callers(), diff)
+		}
+		if diff := Diff(gotArgs, tt.wantArgs); diff != "" {
+			t.Error(Callers(), diff)
+		}
+		if tt.wantParams != nil {
+			if diff := Diff(gotParams, tt.wantParams); diff != "" {
+				t.Error(Callers(), diff)
+			}
+		}
+	}
+
+	t.Run("basic subquery", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		RENTAL, STAFF := NEW_RENTAL(""), NEW_STAFF("s")
+		subquery_rental := NewSubquery("subquery_rental", Postgres.
+			Select(
+				RENTAL.STAFF_ID,
+				Fieldf("COUNT({})", RENTAL.RENTAL_ID).As("rental_count"),
+			).
+			From(RENTAL).
+			GroupBy(RENTAL.STAFF_ID),
+		)
+		tt.item = Postgres.
+			Select(
+				STAFF.STAFF_ID,
+				STAFF.FIRST_NAME,
+				STAFF.LAST_NAME,
+				subquery_rental.Field("rental_count"),
+			).
+			From(STAFF).
+			Join(subquery_rental, subquery_rental.Field("staff_id").Eq(STAFF.STAFF_ID))
+		tt.wantQuery = "SELECT s.staff_id, s.first_name, s.last_name, subquery_rental.rental_count" +
+			" FROM staff AS s" +
+			" JOIN (" +
+			"SELECT rental.staff_id, COUNT(rental.rental_id) AS rental_count" +
+			" FROM rental" +
+			" GROUP BY rental.staff_id" +
+			") AS subquery_rental ON subquery_rental.staff_id = s.staff_id"
+		assert(t, tt)
+	})
+
+	t.Run("subquery nil query", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.item = SQLite.From(NewSubquery("subquery", nil)).Select(FieldLiteral("*"))
+		_, _, _, err := ToSQL("", tt.item)
+		if err == nil {
+			t.Fatal(Callers(), "expected error but got nil")
+		}
+	})
+
+	t.Run("subquery query GetFetchableFields error", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.item = SQLite.From(NewSubquery("subquery", Queryf("SELECT 1"))).Select(FieldLiteral("*"))
+		_, _, _, err := ToSQL("", tt.item)
+		if err == nil {
+			t.Fatal(Callers(), "expected error but got nil")
+		}
+	})
+
+	t.Run("subquery query no fields", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.item = SQLite.From(NewSubquery("subquery", SQLite.Select())).Select(FieldLiteral("*"))
+		_, _, _, err := ToSQL("", tt.item)
+		if err == nil {
+			t.Fatal(Callers(), "expected error but got nil")
+		}
+	})
+
+	t.Run("subquery no alias, dialect == postgres || dialect == mysql", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.item = Postgres.Select(FieldLiteral("*")).From(NewSubquery("", Postgres.Select(FieldValue(1).As("n"))))
+		_, _, _, err := ToSQL("", tt.item)
+		if err == nil {
+			t.Fatal(Callers(), "expected error but got nil")
+		}
+	})
+}
