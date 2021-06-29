411c6a75f7478251411bb4ecd07d6b9d3016d760
bokwoon <bokwoon.c@gmail.com>
2021-06-29 12:18:00 +0800

    checkpoint

--- subquery_test.go
+++ subquery_test.go
@@ -86,7 +86,7 @@ func TestSubquery(t *testing.T) {
 	t.Run("subquery query no fields", func(t *testing.T) {
 		t.Parallel()
 		var tt TT
-		tt.item = SQLite.From(NewSubquery("subquery", SQLite.Select())).Select(FieldLiteral("*"))
+		tt.item = SQLite.From(NewSubquery("subquery", MySQL.Select())).Select(FieldLiteral("*"))
 		_, _, _, err := ToSQL("", tt.item)
 		if err == nil {
 			t.Fatal(Callers(), "expected error but got nil")
@@ -96,7 +96,7 @@ func TestSubquery(t *testing.T) {
 	t.Run("subquery query field no name", func(t *testing.T) {
 		t.Parallel()
 		var tt TT
-		tt.item = SQLite.From(NewSubquery("subquery", SQLite.Select(Value(1)))).Select(FieldLiteral("*"))
+		tt.item = SQLite.From(NewSubquery("subquery", MySQL.Select(Value(1)))).Select(FieldLiteral("*"))
 		_, _, _, err := ToSQL("", tt.item)
 		if err == nil {
 			t.Fatal(Callers(), "expected error but got nil")
@@ -121,5 +121,171 @@ func TestSubquery(t *testing.T) {
 		if err == nil {
 			t.Fatal(Callers(), "expected error but got nil")
 		}
+	})
+}
+
+func Test_SubqueryField(t *testing.T) {
+	type TT struct {
+		dialect                 string
+		item                    SQLExcludeAppender
+		excludedTableQualifiers []string
+		wantQuery               string
+		wantArgs                []interface{}
+		wantParams              map[string][]int
+	}
+
+	assert := func(t *testing.T, tt TT) {
+		gotQuery, gotArgs, gotParams, err := ToSQLExclude(tt.dialect, tt.item, tt.excludedTableQualifiers)
+		if err != nil {
+			t.Fatal(Callers(), err)
+		}
+		if diff := Diff(tt.wantQuery, gotQuery); diff != "" {
+			t.Error(Callers(), diff)
+		}
+		if diff := Diff(gotArgs, tt.wantArgs); diff != "" {
+			t.Error(Callers(), diff)
+		}
+		if tt.wantParams != nil {
+			if diff := Diff(gotParams, tt.wantParams); diff != "" {
+				t.Error(Callers(), diff)
+			}
+		}
+	}
+
+	t.Run("propagate Subquery stickyErr", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", nil)
+		tt.item = q.Field("field")
+		_, _, _, err := ToSQLExclude(tt.dialect, tt.item, tt.excludedTableQualifiers)
+		if err == nil {
+			t.Fatal(Callers(), "expected error but got nil")
+		}
+	})
+
+	t.Run("Subquery field not exists", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("nonexistent_field")
+		_, _, _, err := ToSQLExclude(tt.dialect, tt.item, tt.excludedTableQualifiers)
+		if err == nil {
+			t.Fatal(Callers(), "expected error but got nil")
+		}
+	})
+
+	t.Run("SubqueryField alias", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").As("f")
+		tt.wantQuery = "subquery.field"
+		assert(t, tt)
+	})
+
+	t.Run("SubqueryField ASC NULLS LAST", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").Asc().NullsLast()
+		tt.wantQuery = "subquery.field ASC NULLS LAST"
+		assert(t, tt)
+	})
+
+	t.Run("SubqueryField DESC NULLS FIRST", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").Desc().NullsFirst()
+		tt.wantQuery = "subquery.field DESC NULLS FIRST"
+		assert(t, tt)
+	})
+
+	t.Run("SubqueryField IS NULL", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").IsNull()
+		tt.wantQuery = "subquery.field IS NULL"
+		assert(t, tt)
+	})
+
+	t.Run("SubqueryField IS NOT NULL", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").IsNotNull()
+		tt.wantQuery = "subquery.field IS NOT NULL"
+		assert(t, tt)
+	})
+
+	t.Run("SubqueryField IN (slice)", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").In([]int{5, 6, 7})
+		tt.wantQuery = "subquery.field IN (?, ?, ?)"
+		tt.wantArgs = []interface{}{5, 6, 7}
+		assert(t, tt)
+	})
+
+	t.Run("SubqueryField Eq", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").Eq(123)
+		tt.wantQuery = "subquery.field = ?"
+		tt.wantArgs = []interface{}{123}
+		assert(t, tt)
+	})
+
+	t.Run("SubqueryField Ne", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").Ne(123)
+		tt.wantQuery = "subquery.field <> ?"
+		tt.wantArgs = []interface{}{123}
+		assert(t, tt)
+	})
+
+	t.Run("SubqueryField Gt", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").Gt(123)
+		tt.wantQuery = "subquery.field > ?"
+		tt.wantArgs = []interface{}{123}
+		assert(t, tt)
+	})
+
+	t.Run("SubqueryField Ge", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").Ge(123)
+		tt.wantQuery = "subquery.field >= ?"
+		tt.wantArgs = []interface{}{123}
+		assert(t, tt)
+	})
+
+	t.Run("SubqueryField Lt", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").Lt(123)
+		tt.wantQuery = "subquery.field < ?"
+		tt.wantArgs = []interface{}{123}
+		assert(t, tt)
+	})
+
+	t.Run("SubqueryField Le", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		q := NewSubquery("subquery", MySQL.Select(Value(1).As("field")))
+		tt.item = q.Field("field").Le(123)
+		tt.wantQuery = "subquery.field <= ?"
+		tt.wantArgs = []interface{}{123}
+		assert(t, tt)
 	})
 }
