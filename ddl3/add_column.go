7ab24a8ba81ca4733020e39be4c36295c14e5780
bokwoon <bokwoon.c@gmail.com>
2021-07-06 03:28:28 +0800

    checkpoint

--- ddl3/add_column.go
+++ ddl3/add_column.go
@@ -7,5 +7,8 @@ type AddColumnCommand struct {
 	TableName       string
 	DoIfNotExists   bool
 	Column          Column
-	Constraints     []Constraint
+	Constraints     []Constraint // instead of a fat slice, we just need a specialized set of constraints for SQLite
+	// For SQLite, if INTEGER and PRIMARY KEY and AUOTINCREMENT, we place them
+	// tgt. If other constraints, we treat it as special case. No need for an
+	// entire constraints slice, which will likely take up a lot of space.
 }
