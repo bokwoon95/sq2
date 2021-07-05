4cd83a6acbac700c534060a35a0e77e00a28ec20
bokwoon <bokwoon.c@gmail.com>
2021-07-05 23:38:48 +0800

    checkpoint

--- ddl3/alter_column.go
+++ ddl3/alter_column.go
@@ -3,8 +3,15 @@ package ddl3
 type AlterColumnCommand struct {
 	Dialect         string
 	DoIfTableExists bool
-	AlterType       string // TYPE | SET DEFAULT | DROP DEFAULT | NOT NULL | DROP NULL | DROP EXPRESSION | SET IDENTITY | DROP IDENTITY
-	OldColumnName   string
-	NewColumn       Column
+	AlterType       int // TYPE | SET DEFAULT | DROP DEFAULT | NOT NULL | DROP NULL | DROP EXPRESSION | SET IDENTITY | DROP IDENTITY
+	Column          Column
+	Using           string
 	DoIfExists      bool
 }
+
+// When analyzing the Column fields, we need a magic string that represents
+// "DROP this" for something like ColumnDefault. Basically an empty string
+// means "ignore this field", while the magic string means "actually drop this
+// field". But what magic value to use? NULL "\x00" will not serialize well to
+// JSON. If we are using magic strings, we will not need the AlterType field
+// anymore.
