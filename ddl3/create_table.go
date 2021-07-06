0b81ee33627e75c2d9663f04bb0beb98781b5a0e
bokwoon <bokwoon.c@gmail.com>
2021-07-06 18:36:39 +0800

    checkpoint

--- ddl3/create_table.go
+++ ddl3/create_table.go
@@ -2,6 +2,7 @@ package ddl3
 
 import (
 	"bytes"
+	"fmt"
 
 	"github.com/bokwoon95/sq"
 )
@@ -18,5 +19,30 @@ var _ Command = CreateTableCommand{}
 func (cmd CreateTableCommand) GetType() string { return CREATE_TABLE }
 
 func (cmd CreateTableCommand) AppendSQL(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int) error {
+	if cmd.Table.TableName == "" {
+		return fmt.Errorf("CREATE TABLE: table has no name")
+	}
+	if len(cmd.Table.Columns) == 0 {
+		return fmt.Errorf("CREATE TABLE: table %s has no columns", cmd.Table.TableName)
+	}
+	if cmd.Table.VirtualTable != "" {
+		if dialect != sq.DialectSQLite {
+			return fmt.Errorf("CREATE TABLE: only SQLite has VIRTUAL TABLE support (table=%s)", cmd.Table.TableName)
+		}
+		buf.WriteString("CREATE VIRTUAL TABLE ")
+	} else {
+		buf.WriteString("CREATE TABLE ")
+	}
+	if cmd.DoIfNotExists {
+		buf.WriteString("IF NOT EXISTS ")
+	}
+	if cmd.Table.TableSchema != "" {
+		buf.WriteString(sq.QuoteIdentifier(dialect, cmd.Table.TableSchema) + ".")
+	}
+	buf.WriteString(sq.QuoteIdentifier(dialect, cmd.Table.TableName))
+	if cmd.Table.VirtualTable != "" {
+		buf.WriteString(" USING " + cmd.Table.VirtualTable)
+	}
+	buf.WriteString(" (")
 	return nil
 }
