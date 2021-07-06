52800921b3469fa51d9099adba0bdb25c9fa2c33
bokwoon <bokwoon.c@gmail.com>
2021-07-06 23:44:30 +0800

    checkpoint

--- ddl3/catalog.go
+++ ddl3/catalog.go
@@ -1,6 +1,11 @@
 package ddl3
 
-import "github.com/bokwoon95/sq"
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/bokwoon95/sq"
+)
 
 type Catalog struct {
 	Dialect         string
@@ -56,15 +61,131 @@ func (c *Catalog) LoadTables(tables ...sq.SchemaTable) error {
 	return nil
 }
 
-func (c *Catalog) loadTable(table sq.SchemaTable) error {
+func (c *Catalog) loadTable(table sq.SchemaTable) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			switch r := r.(type) {
+			case error:
+				err = r
+			default:
+				err = fmt.Errorf("panic: " + fmt.Sprint(r))
+			}
+		}
+	}()
+	if table == nil {
+		return fmt.Errorf("table is nil")
+	}
+	tableValue := reflect.ValueOf(table)
+	tableType := tableValue.Type()
+	if tableType.Kind() != reflect.Struct {
+		return fmt.Errorf("table is not a struct")
+	}
+	if tableValue.NumField() == 0 {
+		return fmt.Errorf("table is empty struct")
+	}
+	tableinfo, ok := tableValue.Field(0).Interface().(sq.TableInfo)
+	if !ok {
+		return fmt.Errorf("first field of table struct is not an embedded sq.TableInfo")
+	}
+	if !tableType.Field(0).Anonymous {
+		return fmt.Errorf("first field of table struct is not an embedded sq.TableInfo")
+	}
+	var schema Schema
+	if i := c.CachedSchemaPosition(tableinfo.TableSchema); i >= 0 {
+		schema = c.Schemas[i]
+		defer func(i int) { c.Schemas[i] = schema }(i)
+	} else {
+		schema = Schema{SchemaName: tableinfo.TableSchema}
+		defer func() { c.AppendSchema(schema) }()
+	}
+	var tbl Table
+	if tableinfo.TableName == "" {
+		return fmt.Errorf("table name is empty")
+	}
+	if i := schema.CachedTablePosition(tableinfo.TableName); i >= 0 {
+		tbl = schema.Tables[i]
+		defer func(i int) { schema.Tables[i] = tbl }(i)
+	} else {
+		tbl = Table{TableSchema: tableinfo.TableSchema, TableName: tableinfo.TableName}
+		defer func() { schema.AppendTable(tbl) }()
+	}
+	qualifiedTable := tbl.TableSchema + "." + tbl.TableName
+	if tbl.TableSchema == "" {
+		qualifiedTable = qualifiedTable[1:]
+	}
+	tableModifiers := tableType.Field(0).Tag.Get("ddl")
+	modifiers, _, err := lexModifiers(tableModifiers)
+	if err != nil {
+		return fmt.Errorf("%s: %s", qualifiedTable, err.Error())
+	}
+	for _, modifier := range modifiers {
+		switch modifier[0] {
+		case "virtual":
+			virtualTable, modifiers, _, err := lexValue(modifier[1])
+			if err != nil {
+				return fmt.Errorf("%s: %s", qualifiedTable, err.Error())
+			}
+			tbl.VirtualTable = virtualTable
+			for _, modifier := range modifiers {
+				virtualTableArg := modifier[0]
+				if modifier[1] != "" {
+					virtualTableArg += "=" + modifier[1]
+				}
+				tbl.VirtualTableArgs = append(tbl.VirtualTableArgs, virtualTableArg)
+			}
+		case "primarykey":
+			err = tbl.LoadConstraintConfig(PRIMARY_KEY, tbl.TableSchema, tbl.TableName, nil, modifier[1])
+			if err != nil {
+				return fmt.Errorf("%s: %s", qualifiedTable, err.Error())
+			}
+		case "references":
+			err = tbl.LoadConstraintConfig(FOREIGN_KEY, tbl.TableSchema, tbl.TableName, nil, modifier[1])
+			if err != nil {
+				return fmt.Errorf("%s: %s", qualifiedTable, err.Error())
+			}
+		case "unique":
+			err = tbl.LoadConstraintConfig(UNIQUE, tbl.TableSchema, tbl.TableName, nil, modifier[1])
+			if err != nil {
+				return fmt.Errorf("%s: %s", qualifiedTable, err.Error())
+			}
+		case "check":
+			err = tbl.LoadConstraintConfig(CHECK, tbl.TableSchema, tbl.TableName, nil, modifier[1])
+			if err != nil {
+				return fmt.Errorf("%s: %s", qualifiedTable, err.Error())
+			}
+		case "index":
+			err = tbl.LoadIndexConfig(tbl.TableSchema, tbl.TableName, nil, modifier[1])
+			if err != nil {
+				return fmt.Errorf("%s: %s", qualifiedTable, err.Error())
+			}
+		default:
+			return fmt.Errorf("%s: unknown modifier '%s'", qualifiedTable, modifier[0])
+		}
+	}
+	for i := 1; i < tableValue.NumField(); i++ {
+		field, ok := tableValue.Field(i).Interface().(sq.Field)
+		if !ok {
+			continue
+		}
+		columnName := field.GetName()
+		if columnName == "" {
+			return fmt.Errorf("table %s field #%d has no name", tableinfo.TableName, i)
+		}
+		columnType := defaultColumnType(c.Dialect, field)
+		config := tableType.Field(i).Tag.Get("ddl")
+		err := tbl.LoadColumnConfig(c.Dialect, columnName, columnType, config)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
-func (c *Catalog) LoadIViews(views ...IView) error {
+func (c *Catalog) LoadDDLViews(ddlViews ...DDLView) error {
 	return nil
 }
 
-func (c *Catalog) loadIView(view IView) error {
+func (c *Catalog) loadDDLView(ddlView DDLView) error {
 	return nil
 }
 
