e896826012c8fab5222ef33eeb3f9941acdd4203
bokwoon <bokwoon.c@gmail.com>
2021-07-08 23:58:39 +0800

    checkpoint

--- ddl3/catalog_diff.go
+++ ddl3/catalog_diff.go
@@ -1,66 +1,11 @@
 package ddl3
 
-import (
-	"io"
-)
-
 type CatalogDiff struct {
 	SchemaDiffs      []SchemaDiff
 	schemaDiffsCache map[string]int // 8 bytes
 }
 
 func DiffCatalog(gotCatalog, wantCatalog Catalog) (CatalogDiff, error) {
-	var set CatalogDiff
-	return set, nil
+	var catalogDiff CatalogDiff
+	return catalogDiff, nil
 }
-
-func (set CatalogDiff) Commands() Commands {
-	return Commands{}
-}
-
-func (set CatalogDiff) WriteOut(w io.Writer) error {
-	return nil
-}
-
-type CommandType int
-
-// TODO: For (sqlite,postgres,mysql) does renaming a table:
-// - break triggers?
-// - break views?
-const (
-	// Schema
-	CREATE_SCHEMA CommandType = 1 << iota
-	RENAME_SCHEMA
-	DROP_SCHEMA
-	// Table
-	CREATE_TABLE
-	RENAME_TABLE
-	DROP_TABLE
-	// Column
-	ADD_COLUMN
-	ALTER_COLUMN
-	RENAME_COLUMN
-	DROP_COLUMN
-	// Constraint
-	ADD_CONSTRAINT
-	RENAME_CONSTRAINT
-	DROP_CONSTRAINT
-	// Index
-	CREATE_INDEX
-	RENAME_INDEX
-	DROP_INDEX
-	// Function
-	CREATE_FUNCTION
-	RENAME_FUNCTION
-	DROP_FUNCTION
-	// View
-	CREATE_VIEW
-	RENAME_VIEW
-	DROP_VIEW
-	// Trigger
-	CREATE_TRIGGER
-	RENAME_TRIGGER
-	DROP_TRIGGER
-	// Table DML
-	TABLE_DML
-)
