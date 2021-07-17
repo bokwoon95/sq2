d052efd60c6cc98e9dd602d4af48f504d6024456
bokwoon <bokwoon.c@gmail.com>
2021-07-17 17:45:40 +0800

    checkpoint

--- ddl/index_test.go
+++ ddl/index_test.go
@@ -52,6 +52,24 @@ func Test_CreateIndexCommnd(t *testing.T) {
 		assert(t, tt)
 	})
 
+	t.Run("(dialect == sqlite)", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.dialect = sq.DialectSQLite
+		tt.item = CreateIndexCommand{
+			Index: Index{
+				TableSchema: "my_schema",
+				TableName:   "my_table",
+				IndexName:   "my_index",
+				Columns:     []string{"column a", ""},
+				Exprs:       []string{"", "UPPER(column_b)"},
+				Predicate:   "column_e IS NOT NULL",
+			},
+		}
+		tt.wantQuery = `CREATE INDEX my_index ON my_schema.my_table ("column a", UPPER(column_b)) WHERE column_e IS NOT NULL;`
+		assert(t, tt)
+	})
+
 	t.Run("(dialect == mysql) FULLTEXT", func(t *testing.T) {
 		t.Parallel()
 		var tt TT
@@ -67,5 +85,77 @@ func Test_CreateIndexCommnd(t *testing.T) {
 		}
 		tt.wantQuery = `FULLTEXT INDEX my_index (my_column)`
 		assert(t, tt)
+	})
+
+	t.Run("(dialect != postgres) CREATE INDEX CONCURRENTLY", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.dialect = sq.DialectMySQL
+		tt.item = CreateIndexCommand{
+			CreateConcurrently: true,
+			Index: Index{
+				TableName: "my_table",
+				IndexName: "my_index",
+				Columns:   []string{"my_column"},
+			},
+		}
+		_, _, _, err := sq.ToSQL(tt.dialect, tt.item)
+		if err == nil {
+			t.Fatal(testcallers(), "expected error but got nil")
+		}
+	})
+
+	t.Run("(dialect != postgres && dialect != mysql) index type", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.dialect = sq.DialectSQLite
+		tt.item = CreateIndexCommand{
+			Index: Index{
+				TableName: "my_table",
+				IndexName: "my_index",
+				IndexType: "HASH",
+				Columns:   []string{"my_column"},
+			},
+		}
+		_, _, _, err := sq.ToSQL(tt.dialect, tt.item)
+		if err == nil {
+			t.Fatal(testcallers(), "expected error but got nil")
+		}
+	})
+
+	t.Run("(dialect != postgres) INCLUDE", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.dialect = sq.DialectMySQL
+		tt.item = CreateIndexCommand{
+			Index: Index{
+				TableName:      "my_table",
+				IndexName:      "my_index",
+				Columns:        []string{"my_column"},
+				IncludeColumns: []string{"other_column"},
+			},
+		}
+		_, _, _, err := sq.ToSQL(tt.dialect, tt.item)
+		if err == nil {
+			t.Fatal(testcallers(), "expected error but got nil")
+		}
+	})
+
+	t.Run("(dialect != postgres && dialect != sqlite) WHERE", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.dialect = sq.DialectMySQL
+		tt.item = CreateIndexCommand{
+			Index: Index{
+				TableName: "my_table",
+				IndexName: "my_index",
+				Columns:   []string{"my_column"},
+				Predicate: "other_column IS NULL",
+			},
+		}
+		_, _, _, err := sq.ToSQL(tt.dialect, tt.item)
+		if err == nil {
+			t.Fatal(testcallers(), "expected error but got nil")
+		}
 	})
 }
