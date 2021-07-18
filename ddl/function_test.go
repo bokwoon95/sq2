6ff961fb99365e63933b5429ef1975aa1428ea53
bokwoon <bokwoon.c@gmail.com>
2021-07-18 15:51:59 +0800

    checkpoint

--- ddl/function_test.go
+++ ddl/function_test.go
@@ -67,4 +67,66 @@ func Test_Function(t *testing.T) {
 		tt.wantArgTypes = []string{"integer", "numeric"}
 		assert(t, tt)
 	})
+
+	t.Run("(dialect == postgres)", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.dialect = sq.DialectPostgres
+		tt.item.SQL = `CREATE OR REPLACE FUNCTION double_salary(emp) RETURNS numeric`
+		tt.wantFunctionName = "double_salary"
+		tt.wantArgModes = []string{""}
+		tt.wantArgNames = []string{""}
+		tt.wantArgTypes = []string{"emp"}
+		assert(t, tt)
+	})
+
+	t.Run("(dialect == postgres)", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.dialect = sq.DialectPostgres
+		tt.item.SQL = `CREATE FUNCTION sum_n_product (int, y int, OUT sum int DEFAULT 3, OUT product int=22)`
+		tt.wantFunctionName = "sum_n_product"
+		tt.wantArgModes = []string{"", "", "OUT", "OUT"}
+		tt.wantArgNames = []string{"", "y", "sum", "product"}
+		tt.wantArgTypes = []string{"int", "int", "int", "int"}
+		assert(t, tt)
+	})
+
+	t.Run("(dialect == postgres)", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.dialect = sq.DialectPostgres
+		tt.item.SQL = `CREATE FUNCTION make_array(anyelement, anyelement) RETURNS anyarray`
+		tt.wantFunctionName = "make_array"
+		tt.wantArgModes = []string{"", ""}
+		tt.wantArgNames = []string{"", ""}
+		tt.wantArgTypes = []string{"anyelement", "anyelement"}
+		assert(t, tt)
+	})
+
+	t.Run("(dialect == postgres)", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.dialect = sq.DialectPostgres
+		tt.item.SQL = `
+CREATE OR REPLACE FUNCTION years_compare( IN year1 integer DEFAULT NULL,
+                                          IN year2 integer DEFAULT NULL )`
+		tt.wantFunctionName = "years_compare"
+		tt.wantArgModes = []string{"IN", "IN"}
+		tt.wantArgNames = []string{"year1", "year2"}
+		tt.wantArgTypes = []string{"integer", "integer"}
+		assert(t, tt)
+	})
+
+	t.Run("(dialect == postgres)", func(t *testing.T) {
+		t.Parallel()
+		var tt TT
+		tt.dialect = sq.DialectPostgres
+		tt.item.SQL = `create function foo(bar varchar , baz varchar='qux') returns varchar`
+		tt.wantFunctionName = "foo"
+		tt.wantArgModes = []string{"", ""}
+		tt.wantArgNames = []string{"bar", "baz"}
+		tt.wantArgTypes = []string{"varchar", "varchar"}
+		assert(t, tt)
+	})
 }
