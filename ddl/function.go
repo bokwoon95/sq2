5c714b716bd428119028a185cf9cb523fd08a602
bokwoon <bokwoon.c@gmail.com>
2021-07-19 15:02:59 +0800

    checkpoint

--- ddl/function.go
+++ ddl/function.go
@@ -116,6 +116,9 @@ type CreateFunctionCommand struct {
 }
 
 func (cmd CreateFunctionCommand) AppendSQL(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int) error {
+	if dialect == sq.DialectSQLite {
+		return fmt.Errorf("sqlite does not support functions")
+	}
 	buf.WriteString(cmd.Function.SQL)
 	return nil
 }
@@ -127,10 +130,37 @@ type DropFunctionCommand struct {
 }
 
 func (cmd DropFunctionCommand) AppendSQL(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int) error {
+	if dialect == sq.DialectSQLite {
+		return fmt.Errorf("sqlite does not support functions")
+	}
+	buf.WriteString("DROP FUNCTION ")
+	if cmd.DropIfExists {
+		buf.WriteString("IF EXISTS ")
+	}
+	buf.WriteString(sq.QuoteIdentifier(dialect, cmd.Function.FunctionName))
+	if len(cmd.Function.ArgTypes) > 0 && dialect == sq.DialectPostgres {
+		buf.WriteString(" (" + strings.Join(cmd.Function.ArgTypes, ", ") + ")")
+	}
+	if cmd.DropCascade {
+		buf.WriteString(" CASCADE")
+	}
+	buf.WriteString(";")
 	return nil
 }
 
 type RenameFunctionCommand struct {
 	Function     Function
 	RenameToName string
+}
+
+func (cmd RenameFunctionCommand) AppendSQL(dialect string, buf *bytes.Buffer, args *[]interface{}, params map[string][]int) error {
+	if dialect == sq.DialectSQLite || dialect == sq.DialectMySQL {
+		return fmt.Errorf("%s does not support renaming functions", dialect)
+	}
+	buf.WriteString("ALTER FUNCTION " + sq.QuoteIdentifier(dialect, cmd.Function.FunctionName))
+	if len(cmd.Function.ArgTypes) > 0 && dialect == sq.DialectPostgres {
+		buf.WriteString(" (" + strings.Join(cmd.Function.ArgTypes, ", ") + ")")
+	}
+	buf.WriteString(" RENAME TO " + sq.QuoteIdentifier(dialect, cmd.RenameToName) + ";")
+	return nil
 }
