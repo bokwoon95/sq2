fc26635a723a4b727b1e4ec96a5b7d17ea7f15b0
bokwoon <bokwoon.c@gmail.com>
2021-07-21 22:54:23 +0800

    checkpoint

---
+++ ddl/sql/postgres_triggers.sql
@@ -0,0 +1,15 @@
+SELECT
+    table_namespace.nspname AS table_schema
+    ,table_info.relname AS table_name
+    ,pg_trigger.tgname AS trigger_name
+    ,COALESCE(pg_get_triggerdef(pg_trigger.oid, TRUE) || ';', '') AS sql
+    ,'' AS action_timing
+    ,'' AS event_manipulation
+FROM
+    pg_catalog.pg_trigger
+    JOIN pg_catalog.pg_class AS table_info ON table_info.oid = pg_trigger.tgrelid
+    JOIN pg_catalog.pg_namespace AS table_namespace ON table_namespace.oid = table_info.relnamespace
+WHERE
+    table_namespace.nspname NOT IN ('pg_catalog', 'information_schema', 'pg_toast')
+    AND NOT pg_trigger.tgisinternal
+;
