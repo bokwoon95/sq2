92cd6524d1964743ae829315b6c0326bc7c333b1
bokwoon <bokwoon.c@gmail.com>
2021-07-23 23:41:17 +0800

    checkpoint

---
+++ ddl/sql/mysql_columns.tmpl.sql
@@ -0,0 +1,32 @@
+SELECT
+    table_schema
+    ,table_name
+    ,column_name
+    ,data_type AS column_type_1
+    ,column_type AS column_type_2
+    ,COALESCE(numeric_precision, 0) AS numeric_precision
+    ,COALESCE(numeric_scale, 0) AS numeric_scale
+    ,extra = 'auto_increment' AS is_autoincrement
+    ,'' AS identity
+    ,NOT is_nullable AS is_notnull
+    ,extra = 'DEFAULT_GENERATED on update CURRENT_TIMESTAMP' AS on_update_current_timestamp
+    ,COALESCE(generation_expression, '') AS generated_expr
+    ,CASE extra WHEN 'STORED GENERATED' THEN TRUE ELSE FALSE END AS generated_expr_stored
+    ,COALESCE(collation_name, '') AS collation_name
+    ,COALESCE(column_default, '') AS column_default
+FROM
+    information_schema.columns
+    JOIN information_schema.tables USING (table_schema, table_name)
+WHERE
+    tables.table_type = 'BASE TABLE'
+    {{- if not .OverrideDefaultPredicate }}
+    AND columns.table_schema NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys')
+    {{- end if }}
+    {{- if NOT .CustomPredicate }}
+    AND {{ .CustomPredicate }}
+    {{- end if }}
+    -- user provides something like "{schema} NOT IN ({1}, {2})", "schema_migrations", "schema_versions"
+    -- alternatively: "{schema} NOT IN ({})", []string{"schema_migrations", "schema_versions"}
+    -- sq.Param("schema", sq.Literal("columns.table_schema")) will be appended
+    -- to the end of the args so that it doesn't mess with any ordinal params.
+;
