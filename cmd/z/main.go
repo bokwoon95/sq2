91e49415c1cbbaf237461c0956a9b9f35b30fe25
bokwoon <bokwoon.c@gmail.com>
2021-07-24 00:57:15 +0800

    checkpoint

--- cmd/z/main.go
+++ cmd/z/main.go
@@ -1,21 +1,71 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jackc/pgx/v4"
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	// db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5440/db?sslmode=disable")
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3310)/db")
+	// pgx
+	pgxConfig, err := pgx.ParseConfig("postgres://postgres:postgres@localhost:5442/db?sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	pgxDB, err := pgx.ConnectConfig(context.Background(), pgxConfig)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = pgxDB.Exec(context.Background(), `
+	CREATE TABLE IF NOT EXISTS tmppgx ( id INT, a TEXT, b TIMESTAMPTZ );
+	ALTER TABLE IF EXISTS tmppgx DROP COLUMN IF EXISTS a;
+	ALTER TABLE IF EXISTS tmppgx DROP COLUMN IF EXISTS b;
+	DROP TABLE tmppgx;
+	`)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// pq
+	pqDB, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5442/db?sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = pqDB.Exec(`
+	CREATE TABLE IF NOT EXISTS tmppq ( id INT, a TEXT, b TIMESTAMPTZ );
+	ALTER TABLE IF EXISTS tmppq DROP COLUMN IF EXISTS a;
+	ALTER TABLE IF EXISTS tmppq DROP COLUMN IF EXISTS b;
+	DROP TABLE tmppq;
+	`)
+	// mysql
+	mysqlDB, err := sql.Open("mysql", "root:root@tcp(localhost:3312)/db?multiStatements=true")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = mysqlDB.Exec(`
+	CREATE TABLE IF NOT EXISTS tmp ( id INT, a TEXT, b TIMESTAMPTZ );
+	ALTER TABLE tmp DROP COLUMN a;
+	ALTER TABLE tmp DROP COLUMN b;
+	DROP TABLE tmp;
+	`)
+	// sqlite
+	sqliteDB, err := sql.Open("sqlite3", "./db.sqlite3")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = sqliteDB.Exec(`
+	CREATE TABLE IF NOT EXISTS tmp ( id INT, a TEXT, b TIMESTAMPTZ );
+	ALTER TABLE tmp DROP COLUMN a;
+	ALTER TABLE tmp DROP COLUMN b;
+	DROP TABLE tmp;
+	`)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	// _, err = db.Exec(`CREATE FUNCTION test (text) RETURNS text AS $$ BEGIN RETURN '5'; END; $$ LANGUAGE plpgsql;`)
-	_, err = db.Exec(`CREATE FUNCTION test (t text) RETURNS text DETERMINISTIC BEGIN RETURN '5'; END;`)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// _, err = db.Exec(`CREATE FUNCTION test (t text) RETURNS text DETERMINISTIC BEGIN RETURN '5'; END;`)
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/bokwoon95/sq
 go 1.16
 
 require (
-	github.com/davecgh/go-spew v1.1.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/google/go-cmp v0.5.6
+	github.com/jackc/pgx/v4 v4.12.0
 	github.com/lib/pq v1.10.2
 	github.com/mattn/go-sqlite3 v1.14.8
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 )
