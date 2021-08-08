2bc19f5f14018d57f2a592390585be6a1e0fe2bd
bokwoon <bokwoon.c@gmail.com>
2021-08-08 20:12:08 +0800

    checkpoint

--- ddl2/sql/postgres_enums.sql
+++ ddl2/sql/postgres_enums.sql
@@ -1,22 +1,22 @@
 SELECT
-    pg_namespace.nspname AS enum_schema
+    schemas.nspname AS enum_schema
     ,pg_type.typname AS enum_name
     ,json_agg(pg_enum.enumlabel::TEXT ORDER BY pg_enum.enumsortorder) AS enum_values
 FROM
     pg_enum
     JOIN pg_type ON pg_type.oid = pg_enum.enumtypid
-    JOIN pg_namespace ON pg_namespace.oid = pg_type.typnamespace
+    JOIN pg_namespace AS schemas ON schemas.oid = pg_type.typnamespace
 WHERE
     TRUE
-    {{ if not .IncludeSystemCatalogs }}AND pg_namespace.nspname <> 'information_schema' AND pg_namespace.nspname NOT LIKE 'pg_%'{{ end }}
-    {{ if .WithSchemas }}AND pg_namespace.nspname IN ({{ printList .WithSchemas }}){{ end }}
-    {{ if .WithoutSchemas }}AND pg_namespace.nspname NOT IN ({{ printList .WithoutSchemas }}){{ end }}
+    {{ if not .IncludeSystemCatalogs }}AND schemas.nspname <> 'information_schema' AND schemas.nspname NOT LIKE 'pg_%'{{ end }}
+    {{ if .WithSchemas }}AND schemas.nspname IN ({{ printList .WithSchemas }}){{ end }}
+    {{ if .WithoutSchemas }}AND schemas.nspname NOT IN ({{ printList .WithoutSchemas }}){{ end }}
 GROUP BY
-    pg_namespace.nspname
+    schemas.nspname
     ,pg_type.typname
 {{- if .SortOutput }}
 ORDER BY
-    pg_namespace.nspname
+    schemas.nspname
     ,pg_type.typname
 {{- end }}
 ;
