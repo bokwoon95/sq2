8312238ff5700bf5901aca15f4986ea34f23c4ed
bokwoon <bokwoon.c@gmail.com>
2021-08-08 17:13:29 +0800

    checkpoint

--- ddl2/sql/postgres_tables.sql
+++ ddl2/sql/postgres_tables.sql
@@ -1,18 +1,19 @@
 SELECT
-    table_schema
-    ,table_name
+    table_namespace.nspname AS table_schema
+    ,table_info.relname AS table_name
 FROM
-    information_schema.tables
+    pg_catalog.pg_namespace AS table_namespace
+    JOIN pg_catalog.pg_class AS table_info ON table_info.relkind = 'r' AND table_info.relnamespace = table_namespace.oid 
 WHERE
-    table_type = 'BASE TABLE'
-    {{ if not .IncludeSystemCatalogs }}AND table_schema <> 'information_schema' AND table_schema NOT LIKE 'pg_%'{{ end }}
-    {{ if .WithSchemas }}AND table_schema IN ({{ printList .WithSchemas }}){{ end }}
-    {{ if .WithoutSchemas }}AND table_schema NOT IN ({{ printList .WithoutSchemas }}){{ end }}
-    {{ if .WithTables }}AND table_name IN ({{ printList .WithTables }}){{ end }}
-    {{ if .WithoutTables }}AND table_name NOT IN ({{ printList .WithoutTables }}){{ end }}
+    TRUE
+    {{ if not .IncludeSystemCatalogs }}AND table_namespace.nspname <> 'information_schema' AND table_namespace.nspname NOT LIKE 'pg_%'{{ end }}
+    {{ if .WithSchemas }}AND table_namespace.nspname IN ({{ printList .WithSchemas }}){{ end }}
+    {{ if .WithoutSchemas }}AND table_namespace.nspname NOT IN ({{ printList .WithoutSchemas }}){{ end }}
+    {{ if .WithTables }}AND table_info.relname IN ({{ printList .WithTables }}){{ end }}
+    {{ if .WithoutTables }}AND table_info.relname NOT IN ({{ printList .WithoutTables }}){{ end }}
 {{- if .SortOutput }}
 ORDER BY
-    table_schema
-    ,table_name
+    table_namespace.nspname
+    ,table_info.relname
 {{- end }}
 ;
