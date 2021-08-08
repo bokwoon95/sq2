49b3e011975cd8b47079858fb8dda3454a3d54fe
bokwoon <bokwoon.c@gmail.com>
2021-08-08 20:23:30 +0800

    checkpoint

--- ddl2/sql/postgres_tables.sql
+++ ddl2/sql/postgres_tables.sql
@@ -1,19 +1,19 @@
 SELECT
-    table_namespace.nspname AS table_schema
-    ,table_info.relname AS table_name
+    schemas.nspname AS table_schema
+    ,tables.relname AS table_name
 FROM
-    pg_namespace AS table_namespace
-    JOIN pg_class AS table_info ON table_info.relkind = 'r' AND table_info.relnamespace = table_namespace.oid 
+    pg_class AS tables
+    JOIN pg_namespace AS schemas ON schemas.oid = tables.relnamespace
 WHERE
-    TRUE
-    {{ if not .IncludeSystemCatalogs }}AND table_namespace.nspname <> 'information_schema' AND table_namespace.nspname NOT LIKE 'pg_%'{{ end }}
-    {{ if .WithSchemas }}AND table_namespace.nspname IN ({{ printList .WithSchemas }}){{ end }}
-    {{ if .WithoutSchemas }}AND table_namespace.nspname NOT IN ({{ printList .WithoutSchemas }}){{ end }}
-    {{ if .WithTables }}AND table_info.relname IN ({{ printList .WithTables }}){{ end }}
-    {{ if .WithoutTables }}AND table_info.relname NOT IN ({{ printList .WithoutTables }}){{ end }}
+    tables.relkind = 'r'
+    {{ if not .IncludeSystemCatalogs }}AND schemas.nspname <> 'information_schema' AND schemas.nspname NOT LIKE 'pg_%'{{ end }}
+    {{ if .WithSchemas }}AND schemas.nspname IN ({{ printList .WithSchemas }}){{ end }}
+    {{ if .WithoutSchemas }}AND schemas.nspname NOT IN ({{ printList .WithoutSchemas }}){{ end }}
+    {{ if .WithTables }}AND tables.relname IN ({{ printList .WithTables }}){{ end }}
+    {{ if .WithoutTables }}AND tables.relname NOT IN ({{ printList .WithoutTables }}){{ end }}
 {{- if .SortOutput }}
 ORDER BY
-    table_namespace.nspname
-    ,table_info.relname
+    schemas.nspname
+    ,tables.relname
 {{- end }}
 ;
