43437a62eeac745e717fedce16bf8785b5ea3d59
bokwoon <bokwoon.c@gmail.com>
2021-08-08 20:14:51 +0800

    checkpoint

--- ddl2/sql/postgres_functions.sql
+++ ddl2/sql/postgres_functions.sql
@@ -1,33 +1,33 @@
 SELECT
-    pg_namespace.nspname AS function_schema
-    ,pg_proc.proname AS function_name
-    ,pg_get_functiondef(pg_proc.oid) AS sql
-    ,pg_get_function_arguments(pg_proc.oid) AS raw_args
-    ,pg_get_function_result(pg_proc.oid) AS return_type
+    schemas.nspname AS function_schema
+    ,proc1.proname AS function_name
+    ,pg_get_functiondef(proc1.oid) AS sql
+    ,pg_get_function_arguments(proc1.oid) AS raw_args
+    ,pg_get_function_result(proc1.oid) AS return_type
 FROM
-    pg_proc
-    JOIN pg_namespace ON pg_proc.pronamespace = pg_namespace.oid
+    pg_proc AS proc1
+    JOIN pg_namespace AS schemas ON schemas.oid = proc1.pronamespace
 WHERE
-    pg_function_is_visible(pg_proc.oid)
+    pg_function_is_visible(proc1.oid)
     AND NOT EXISTS (
         SELECT
             *
         FROM
             pg_extension
             JOIN pg_depend ON pg_depend.refobjid = pg_extension.oid
-            JOIN pg_proc AS extension_proc ON extension_proc.oid = pg_depend.objid
+            JOIN pg_proc AS proc2 ON proc2.oid = pg_depend.objid
         WHERE
             pg_depend.deptype = 'e'
-            AND extension_proc.oid = pg_proc.oid
+            AND proc1.oid = proc2.oid
     )
-    {{ if not .IncludeSystemCatalogs }}AND pg_namespace.nspname <> 'information_schema' AND pg_namespace.nspname NOT LIKE 'pg_%'{{ end }}
-    {{ if .WithSchemas }}AND pg_namespace.nspname IN ({{ printList .WithSchemas }}){{ end }}
-    {{ if .WithoutSchemas }}AND pg_namespace.nspname NOT IN ({{ printList .WithoutSchemas }}){{ end }}
-    {{ if .WithFunctions }}AND pg_proc.proname IN ({{ printList .WithFunctions }}){{ end }}
-    {{ if .WithoutFunctions }}AND pg_proc.proname NOT IN ({{ printList .WithoutFunctions }}){{ end }}
+    {{ if not .IncludeSystemCatalogs }}AND schemas.nspname <> 'information_schema' AND schemas.nspname NOT LIKE 'pg_%'{{ end }}
+    {{ if .WithSchemas }}AND schemas.nspname IN ({{ printList .WithSchemas }}){{ end }}
+    {{ if .WithoutSchemas }}AND schemas.nspname NOT IN ({{ printList .WithoutSchemas }}){{ end }}
+    {{ if .WithFunctions }}AND proc1.proname IN ({{ printList .WithFunctions }}){{ end }}
+    {{ if .WithoutFunctions }}AND proc1.proname NOT IN ({{ printList .WithoutFunctions }}){{ end }}
 {{- if .SortOutput }}
 ORDER BY
-    pg_namespace.nspname
-    ,pg_proc.proname
+    schemas.nspname
+    ,proc1.proname
 {{- end }}
 ;
