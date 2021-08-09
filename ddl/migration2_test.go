573cad295b18fa6c581f0b93818387a43343edad
bokwoon <bokwoon.c@gmail.com>
2021-08-09 19:12:44 +0800

    checkpoint

--- ddl/migration2_test.go
+++ ddl/migration2_test.go
@@ -323,3 +323,130 @@ func Test_Sakila2Postgres(t *testing.T) {
 		t.Fatal(testcallers(), diff)
 	}
 }
+
+func Test_Sakila2MySQL(t *testing.T) {
+	const dialect = sq.DialectMySQL
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3312)/db")
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	err = AutoMigrate2(dialect, db, DropExtraneous|DropCascade)
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	dbi, err := NewDatabaseIntrospector(dialect, db, nil)
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	tbls, err := dbi.GetTables(context.Background(), nil)
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	if len(tbls) != 0 {
+		t.Fatal(testcallers(), " AutoMigrate did not drop all tables:", fmt.Sprint(tbls))
+	}
+	views, err := dbi.GetViews(context.Background(), nil)
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	if len(views) != 0 {
+		t.Fatal(testcallers(), " AutoMigrate did not drop all views:", fmt.Sprint(views))
+	}
+	wantCatalog, err := NewCatalog(dialect, WithTables(
+		NEW_ACTOR(""),
+		NEW_ADDRESS(""),
+		NEW_CATEGORY(""),
+		NEW_CITY(""),
+		NEW_COUNTRY(""),
+		NEW_CUSTOMER(""),
+		NEW_FILM(""),
+		NEW_FILM_ACTOR(""),
+		NEW_FILM_ACTOR_REVIEW(""),
+		NEW_FILM_CATEGORY(""),
+		NEW_FILM_TEXT(""),
+		NEW_INVENTORY(""),
+		NEW_LANGUAGE(""),
+		NEW_PAYMENT(""),
+		NEW_RENTAL(""),
+		NEW_STAFF(""),
+		NEW_STORE(""),
+	), WithDDLViews(
+		NEW_ACTOR_INFO(""),
+		NEW_CUSTOMER_LIST(""),
+		NEW_FILM_LIST(""),
+		NEW_FULL_ADDRESS(""),
+		NEW_NICER_BUT_SLOWER_FILM_LIST(""),
+		NEW_SALES_BY_FILM_CATEGORY(""),
+		NEW_SALES_BY_STORE(""),
+		NEW_STAFF_LIST(""),
+	))
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	upMigration, err := Migrate2(CreateMissing|UpdateExisting, Catalog{}, wantCatalog)
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	buf := bufpool.Get().(*bytes.Buffer)
+	defer func() {
+		buf.Reset()
+		bufpool.Put(buf)
+	}()
+	err = upMigration.WriteSQL(buf)
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	gotUpSQL := buf.String()
+	b, err := fs.ReadFile(embeddedFiles, "sql/mysql_sakila_up.sql")
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	wantUpSQL := strings.TrimSpace(string(b))
+	if diff := testdiff(gotUpSQL, wantUpSQL); diff != "" {
+		t.Fatal(testcallers(), diff)
+	}
+	err = upMigration.Exec(db)
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	gotCatalog, err := NewCatalog(dialect, WithDB(db, &Filter{SortOutput: true}))
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	introspectMigration, err := Migrate2(CreateMissing|UpdateExisting, Catalog{}, gotCatalog)
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	buf.Reset()
+	err = introspectMigration.WriteSQL(buf)
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	gotIntrospectSQL := buf.String()
+	b, err = fs.ReadFile(embeddedFiles, "sql/mysql_sakila_introspect.sql")
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	wantIntrospectSQL := strings.TrimSpace(string(b))
+	if diff := testdiff(gotIntrospectSQL, wantIntrospectSQL); diff != "" {
+		t.Fatal(testcallers(), diff)
+	}
+	downMigration, err := Migrate2(DropExtraneous|DropCascade, gotCatalog, Catalog{})
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	buf.Reset()
+	err = downMigration.WriteSQL(buf)
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	gotDownSQL := buf.String()
+	b, err = fs.ReadFile(embeddedFiles, "sql/mysql_sakila_down.sql")
+	if err != nil {
+		t.Fatal(testcallers(), err)
+	}
+	wantDownSQL := strings.TrimSpace(string(b))
+	if diff := testdiff(gotDownSQL, wantDownSQL); diff != "" {
+		t.Fatal(testcallers(), diff)
+	}
+}
