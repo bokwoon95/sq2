7e27761b8ecbc4b2d8f49a3af817dbc7d51ae9fb
bokwoon <bokwoon.c@gmail.com>
2021-08-11 16:20:57 +0800

    checkpoint

---
+++ fetch_test.go
@@ -0,0 +1,54 @@
+package sq
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+
+	"github.com/bokwoon95/sq/internal/testutil"
+)
+
+func TestSQLiteFetch(t *testing.T) {
+	db, err := sql.Open("sqlite3", *sqliteDSNFlag)
+	if err != nil {
+		t.Fatal(testutil.Callers(), err)
+	}
+
+	t.Run("SELECT DISTINCT", func(t *testing.T) {
+		t.Parallel()
+		ACTOR := xNEW_ACTOR("")
+		var gotLastNames []string
+		rowCount, err := Fetch(Log(db), SQLite.SelectDistinct().From(ACTOR).OrderBy(ACTOR.LAST_NAME).Limit(4), func(row *Row) {
+			lastName := row.String(ACTOR.LAST_NAME)
+			row.Process(func() error {
+				gotLastNames = append(gotLastNames, lastName)
+				return nil
+			})
+		})
+		if err != nil {
+			t.Fatal(testutil.Callers(), err)
+		}
+		if rowCount != 4 {
+			t.Fatal(testutil.Callers(), fmt.Sprintf("got=%d want=%d", rowCount, 4))
+		}
+		wantLastNames := []string{"AKROYD", "ALLEN", "ASTAIRE", "BACALL"}
+		if diff := testutil.Diff(gotLastNames, wantLastNames); diff != "" {
+			t.Fatal(testutil.Callers(), diff)
+		}
+	})
+
+	t.Run("SELECT EXISTS", func(t *testing.T) {
+		t.Parallel()
+		ACTOR := xNEW_ACTOR("")
+		exists, err := FetchExists(Log(db), SQLite.From(ACTOR).Where(Or(
+			ACTOR.FIRST_NAME.EqString("SCARLETT"),
+			ACTOR.LAST_NAME.EqString("JOHANSSON"),
+		)))
+		if err != nil {
+			t.Fatal(testutil.Callers(), err)
+		}
+		if !exists {
+			t.Fatal(testutil.Callers(), fmt.Sprintf("got=%v want=%v", exists, true))
+		}
+	})
+}
